b6515b37002894f74d6de83dfbbebf59fcc974b2
Joshua-timothy <Joshuatimothy.dev@gmail.com>
2023-12-16 15:36:26 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: "'Inter', sans-serif;",
+        PTsans: "'PT Sans', sans-serif",
+        NotoSans: "'Noto Sans KR', sans-serif",
+      },
+      colors: {
+        LCyan: "#2FB2AC",
+        DPurple: "#201C3C",
+        CWhite: "#FFFCFB",
+      },
+    },
+  },
+  plugins: [],
+};
